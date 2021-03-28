5cc73531b6511ccf2ad82e2345ee0b5c493f3beb
Andrew Kvapil <viluon@espiv.net>
2021-03-29 01:07:45 +0200

    Add algorithm explanation

--- Ep00/Main-solution.hs
+++ Ep00/Main-solution.hs
@@ -15,6 +15,10 @@ recipFacts' = map (\i -> 1 / fact i) [0..]
     fact 0 = 1
     fact n = fromIntegral n * fact (n - 1)
 
+fact' :: Int -> Double
+fact' n = fromIntegral $ product [1..n]
+-- >>> fact' 0
+
 -- >>> take 5 $ map (1 /) recipFacts'
 
 -- >>> sum $ takeWhile (> 0) recipFacts'
--- Ep00/Main.hs
+++ Ep00/Main.hs
@@ -13,32 +13,92 @@ where
 přes několik
 řádků -}
 
+
+
+
+
 -- definice začínají nepovinnou typovou anotací, která patří
 -- k celému symbolu (ne např. k jednotlivým argumentům funkce)
 foo :: String
 -- ... a pokračují několika rovnicemi (pro konstanty je jen jedna)
 foo = "foo"
 
+n = 20
+
+message :: String
+message = if n <= 16 then "less than 17"
+          else if n /= 20 then "not twenty"
+          else "it's 20"
+
+message' :: String -- stringy jsou jen seznamy charakterů
+message' = ['F', 'L', 'P']
+
+
+
+
+grades = ['A' .. 'F']
+
+
+
+
+
+
+
+
+
 bar = f 4
   where
-    -- typ funkcí je (->), narozdíl od matematického zápisu (f: ℝ -> ℝ) píšeme čtyřtečku
+    -- typ funkcí je (->), narozdíl od matematického zápisu (f: ℝ -> ℝ)
+    -- píšeme čtyřtečku
     f :: Double -> Double
---  ^ ^^ ^^^^^^ ^^ ^^^^^^ kodoména
+--  ^ ^^ ^^^^^^ ^^ ^^^^^^ obor hodnot
 --  | |  |      |
 --  | |  |      funkce
---  | |  doména
+--  | |  definiční obor
 --  | "je typu"
 --  název
     f x = x^2
---  ^ ^   ^^^ tělo funkce
+--  ^ ^   ^^^ tělo funkce je zároveň návratová hodnota
 --  | |
 --  | parametr(y)
 --  znovu název
+
+
+
+
+
+
+
+
+
+isEven' :: Int -> Bool
+isEven' 0 = True
+isEven' n = if n < 0 then isEven' (-n)
+            else          not (isEven' (n - 1))
+
+
+
+
+
+
+
+
+
 
 isEven :: Int -> Bool
 isEven 0         = True
 isEven n | n < 0 = isEven (-n)
 isEven n         = not (isEven (n - 1))
+--     ^ volné proměnné na levé straně vážou konkrétní hodnoty
+
+
+
+
+
+
+
+
+
 
 -- typový alias, Unicode podpora
 type CeléČíslo = Int
@@ -97,6 +157,31 @@ inc = (+) 1
 
 -- >>> inc 23_208
 
+--             (((f a) b) c) d
+-- Int -> (Int -> (Int -> (Int -> Int)))
+
+curry'' :: ((a, b) -> c) -> (a -> (b -> c))
+curry'' f = \a -> \b -> f(a, b)
+
+curry'  :: ((a, b) -> c) -> a -> b -> c
+curry'  f a b = f (a, b)
+
+
+
+add'' = curry' plus
+
+inc'' = curry' plus 1
+
+
+-- n-tice se skládají ze staticky známého přirozeného čísla libovolných typů
+type PixelRGB     = (Int, Int, Int)
+type Point2D      = (Double, Double)
+type Entry        = (String, Int)
+type StringIntMap = [Entry]
+
+mag :: Point2D -> Double
+mag (x, y) = sqrt (x^2 + y^2)
+
 {-
 Část II: Část II
 ~~~~~~~~~~~~~~~~
@@ -126,6 +211,9 @@ recipFacts'' = 1 : 1 : 1 / 2 : 1 / 6 : 1 / 24 : 1 / 120 : []
 recipFacts' :: [Double]
 recipFacts' = map (\i -> 1 / fact i) [0..]
   where
+    toDouble :: Int -> Double
+    toDouble = fromIntegral
+
     fact :: Int -> Double
     fact = error "to-do"
 
@@ -133,6 +221,15 @@ recipFacts :: [Double]
 recipFacts = go 1
   where
     go k = 1 : map (/ k) (go (k + 1))
+
+--     seznam = 1  :  (1  :  (1  :  (1  :  (...))))
+--          k = 1,     2,     3,     4,     ...
+-- jmenovatel = 1,     2,     3*2,   4*3*2, ...
+
+-- 1/1  : 1/1  : 1/1  : 1/1  : ...
+-- 1/1  : 1/2  : 1/2  : 1/2  : ...
+-- 1/1  : 1/2  : 1/6  : 1/6  : ...
+-- 1/1  : 1/2  : 1/6  : 1/24 : ...
 
 -- >>> sum $ takeWhile (> 0) recipFacts'''
 
